acb381a2d528c6afdd124c362c91904719d3b124
math231k <54638811+math231k@users.noreply.github.com>
2021-05-10 10:13:16 +0200

    added some astoroids

--- Assets/Scripts/AstoroidScripts/AstoroidScript.cs
+++ Assets/Scripts/AstoroidScripts/AstoroidScript.cs
@@ -2,17 +2,48 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AstoroidScrit : MonoBehaviour
+public class AstoroidScript : MonoBehaviour
 {
+    public float maxThrust;
+    public float maxTorque;
+    public float screenTop;
+    public float screenBottom;
+    public float screenLeft;
+    public float screenRight;
+    public Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Vector2 thrust = new Vector2(Random.Range(-maxThrust, maxThrust), Random.Range(-maxThrust, maxThrust));
+        float torque = Random.Range(-maxTorque, maxTorque);
+
+        rb.AddForce(thrust);
+        rb.AddTorque(torque);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Screen Wraping
+        Vector2 newPostition = transform.position;
+        if (transform.position.y > screenTop)
+        {
+            newPostition.y = screenBottom;
+        }
+        if (transform.position.y < screenBottom)
+        {
+            newPostition.y = screenTop;
+        }
+        if (transform.position.x > screenRight)
+        {
+            newPostition.x = screenLeft;
+        }
+        if (transform.position.x < screenLeft)
+        {
+            newPostition.x = screenRight;
+        }
+
+        transform.position = newPostition;
     }
 }
